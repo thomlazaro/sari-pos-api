26d4164b622fe7e74ec64b036b51dba13c11fcb8
Thom <72341679+thomlazaro@users.noreply.github.com>
2022-11-26 15:00:31 +0800

    Delete db config file on repo

--- util/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('sari-pos-db','root','9nub5M81godR',{
-    dialect:'mysql',
-    host:'localhost'
-});
-
-
-module.exports = sequelize;
